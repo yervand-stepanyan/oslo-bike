784defc9a67aaae947a6f7c9bb5d36f41d5350d4
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-10-15 21:06:47 +0200

    Add Buttons component

---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
+import LocalParkingIcon from '@material-ui/icons/LocalParking';
+
+import { useStore } from '../../state/useStore';
+import { useStyles } from './Buttons.style';
+
+function Buttons() {
+  const classes = useStyles();
+  const { isBikeActive, handleBikeSelect, handleParkingSelect } = useStore();
+
+  return (
+    <div className={classes.buttonsContainer}>
+      <ButtonGroup
+        aria-label="outlined primary button group"
+        className={classes.buttonGroup}
+        color="primary"
+        fullWidth
+        variant="contained"
+      >
+        <Button
+          className={isBikeActive ? classes.activeBtn : classes.defaultBtn}
+          onClick={handleBikeSelect}
+        >
+          <DirectionsBikeIcon />
+        </Button>
+        <Button
+          className={!isBikeActive ? classes.activeBtn : classes.defaultBtn}
+          onClick={handleParkingSelect}
+        >
+          <LocalParkingIcon />
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default Buttons;
---
+++ src/components/Buttons/Buttons.style.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+  buttonsContainer: {
+    height: '6vh',
+  },
+  buttonGroup: {
+    height: '100%',
+  },
+  activeBtn: {
+    backgroundColor: '#005FC9',
+  },
+  defaultBtn: {
+    backgroundColor: '#7fafe4',
+  },
+});
