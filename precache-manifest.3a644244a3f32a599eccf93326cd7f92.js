57bdd579d64925ac29284691dba0049d2dad28de
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-10-16 14:50:50 +0200

    Updates

---
+++ precache-manifest.3a644244a3f32a599eccf93326cd7f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83275ca8d7ca2a50eb894f43876b6fe",
+    "url": "/oslo-bike/index.html"
+  },
+  {
+    "revision": "80cfff6e66bc6bf139aa",
+    "url": "/oslo-bike/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e0905929274e26a21e45",
+    "url": "/oslo-bike/static/js/2.58a3ad9e.chunk.js"
+  },
+  {
+    "revision": "14dc907096ff79e6b7be55307d0260dd",
+    "url": "/oslo-bike/static/js/2.58a3ad9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80cfff6e66bc6bf139aa",
+    "url": "/oslo-bike/static/js/main.b5ff6244.chunk.js"
+  },
+  {
+    "revision": "e4e67fcf5b82777719bc",
+    "url": "/oslo-bike/static/js/runtime-main.2f2f8afa.js"
+  }
+]);
